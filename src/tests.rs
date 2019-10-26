9407a543fd8d07d1b87e57e27e1b57fa5db954b2
Alexander Jackson <alexanderjackson@protonmail.com>
2019-10-26 13:22:33 +0100

    Add tests file for unit testing parser

---
+++ src/tests.rs
@@ -0,0 +1,28 @@
+use super::*;
+
+#[test]
+fn ignore_whitespace_test() {
+    let input: &str = r#"
+    name =    4;
+        base = 3;
+    "#;
+
+    let ast = parser::ProgramParser::new().parse(lexer::Lexer::new(input)).unwrap();
+
+    let expected_ast = ast::Program {
+        stmts: vec![
+            ast::Stmt::Statement (
+                ast::Identifier::Name { name: "name" },
+                ast::Operator::Assign,
+                ast::Number::Integer { value: 4 }
+            ),
+            ast::Stmt::Statement (
+                ast::Identifier::Name { name: "base" },
+                ast::Operator::Assign,
+                ast::Number::Integer { value: 3 }
+            )
+        ]
+    };
+
+    assert_eq!(&ast, &expected_ast);
+}
