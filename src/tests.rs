d7a9599522c945c05ef96c36eaf982ba465a1158
Alexander Jackson <alexanderjackson@protonmail.com>
2019-10-26 16:22:45 +0100

    Format with cargo fmt

--- src/tests.rs
+++ src/tests.rs
@@ -7,21 +7,23 @@ fn ignore_whitespace_test() {
         base = 3;
     "#;
 
-    let ast = parser::ProgramParser::new().parse(lexer::Lexer::new(input)).unwrap();
+    let ast = parser::ProgramParser::new()
+        .parse(lexer::Lexer::new(input))
+        .unwrap();
 
     let expected_ast = ast::Program {
         stmts: vec![
-            ast::Stmt::Statement (
+            ast::Stmt::Statement(
                 ast::Identifier::Name { name: "name" },
                 ast::Operator::Assign,
-                ast::Number::Integer { value: 4 }
+                ast::Number::Integer { value: 4 },
             ),
-            ast::Stmt::Statement (
+            ast::Stmt::Statement(
                 ast::Identifier::Name { name: "base" },
                 ast::Operator::Assign,
-                ast::Number::Integer { value: 3 }
-            )
-        ]
+                ast::Number::Integer { value: 3 },
+            ),
+        ],
     };
 
     assert_eq!(&ast, &expected_ast);
