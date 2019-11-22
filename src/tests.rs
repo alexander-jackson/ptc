086fef6f0cececbc8cf806105bf9f030a033e213
Alexander Jackson <alexanderjackson@protonmail.com>
2019-11-22 14:08:43 +0000

    Fix tests to work now indentation is lexed

--- src/tests.rs
+++ src/tests.rs
@@ -1,24 +1,8 @@
 use super::*;
 
 #[test]
-fn ignore_whitespace_test() {
-    let input: &str = r#"
-
-        
-     
-    "#;
-
-    let ast = parser::ProgramParser::new()
-        .parse(lexer::Lexer::new(input.char_indices()));
-
-    assert!(ast.is_ok());
-}
-
-#[test]
 fn allow_underscores_in_identifiers_test() {
-    let input: &str = r#"
-    longer_name
-    "#;
+    let input: &str = r#"longer_name"#;
 
     let ast = parser::ProgramParser::new()
         .parse(lexer::Lexer::new(input.char_indices()));
@@ -28,11 +12,7 @@ fn allow_underscores_in_identifiers_test() {
 
 #[test]
 fn parse_integers_test() {
-    let input: &str = r#"
-    40
-    123
-    23525
-    "#;
+    let input: &str = r#"40"#;
 
     let ast = parser::ProgramParser::new()
         .parse(lexer::Lexer::new(input.char_indices()));
@@ -42,9 +22,7 @@ fn parse_integers_test() {
 
 #[test]
 fn parse_expressions_test() {
-    let input: &str = r#"
-    var = 1 / 1 * 1 + 1 - 1
-    "#;
+    let input: &str = r#"var = 1 / 1 * 1 + 1 - 1"#;
 
     let ast = parser::ProgramParser::new().parse(lexer::Lexer::new(input.char_indices()));
 
@@ -53,9 +31,7 @@ fn parse_expressions_test() {
 
 #[test]
 fn parse_bracketed_expression_test() {
-    let input: &str = r#"
-    var = (1 / 1) * 1
-    "#;
+    let input: &str = r#"var = (1 / 1) * 1"#;
 
     let ast = parser::ProgramParser::new().parse(lexer::Lexer::new(input.char_indices()));
 
@@ -64,9 +40,7 @@ fn parse_bracketed_expression_test() {
 
 #[test]
 fn parse_identifier_expression_test() {
-    let input: &str = r#"
-    var = var_one + var_two
-    "#;
+    let input: &str = r#"var = var_one + var_two"#;
 
     let ast = parser::ProgramParser::new().parse(lexer::Lexer::new(input.char_indices()));
 
@@ -75,9 +49,7 @@ fn parse_identifier_expression_test() {
 
 #[test]
 fn parse_logical_and_expression_test() {
-    let input: &str = r#"
-    variable and other_variable
-    "#;
+    let input: &str = r#"variable and other_variable"#;
 
     let ast = parser::ProgramParser::new().parse(lexer::Lexer::new(input.char_indices()));
 
@@ -86,9 +58,7 @@ fn parse_logical_and_expression_test() {
 
 #[test]
 fn parse_logical_or_expression_test() {
-    let input: &str = r#"
-    variable or other_variable
-    "#;
+    let input: &str = r#"variable or other_variable"#;
 
     let ast = parser::ProgramParser::new().parse(lexer::Lexer::new(input.char_indices()));
 
@@ -97,9 +67,7 @@ fn parse_logical_or_expression_test() {
 
 #[test]
 fn parse_logical_not_expression_test() {
-    let input: &str = r#"
-    not other_variable
-    "#;
+    let input: &str = r#"not other_variable"#;
 
     let ast = parser::ProgramParser::new().parse(lexer::Lexer::new(input.char_indices()));
 
@@ -108,9 +76,20 @@ fn parse_logical_not_expression_test() {
 
 #[test]
 fn parse_pass_statement_test() {
+    let input: &str = r#"pass"#;
+
+    let ast = parser::ProgramParser::new().parse(lexer::Lexer::new(input.char_indices()));
+
+    assert!(ast.is_ok());
+}
+
+#[test]
+fn parse_if_statement_test() {
     let input: &str = r#"
+if 1:
     pass
-    "#;
+
+"#;
 
     let ast = parser::ProgramParser::new().parse(lexer::Lexer::new(input.char_indices()));
 
