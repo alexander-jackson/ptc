fa5805b1113c2fcabeef6c6d6321fd71d334f3f4
Alexander Jackson <alexanderjackson@protonmail.com>
2019-12-04 10:49:14 +0000

    Add support for more operators

--- src/ast/operator.rs
+++ src/ast/operator.rs
@@ -25,8 +25,14 @@ pub enum Operator {
 impl Generate for Operator {
     fn generate(&self) -> String {
         match self {
+            Operator::LogicalOr => String::from("||"),
+            Operator::LogicalAnd => String::from("&&"),
+            Operator::LogicalNot => String::from("!"),
+            Operator::Assign => String::from("="),
             Operator::Plus => String::from("+"),
             Operator::Minus => String::from("-"),
+            Operator::Multiply => String::from("*"),
+            Operator::Divide => String::from("/"),
             _ => String::from(""),
         }
     }
