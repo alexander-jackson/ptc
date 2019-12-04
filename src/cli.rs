98f8bef232be94302dcef19f98d014fe9e2fd99a
Alexander Jackson <alexanderjackson@protonmail.com>
2019-12-04 12:31:15 +0000

    Move user handling code to cli.rs

---
+++ src/cli.rs
@@ -0,0 +1,45 @@
+use std::fs;
+use std::error::Error;
+
+use crate::ast;
+use crate::parser;
+use crate::lexer;
+
+use crate::ast::Generate;
+
+pub struct Args {
+    /// File input using -f <filename> of --filename <filename>
+    pub filename: Option<String>,
+}
+
+pub fn get_arguments() -> Result<Args, Box<dyn Error>> {
+    let mut args = pico_args::Arguments::from_env();
+
+    let args = Args {
+        filename: args.opt_value_from_str(["-f", "--filename"])?,
+    };
+
+    Ok(args)
+}
+
+pub fn process_args(args: Args) -> Result<(), Box<dyn Error>> {
+    let filename = args.filename
+        .expect("Please supply a filename with [-f/--filename]");
+
+    let program_code: String = fs::read_to_string(&filename).expect("Failed to read the file.");
+
+    let ast = parse(&program_code).expect("Failed to parse the given program");
+    dbg!(&ast);
+
+    println!("Program: {}", ast.generate());
+
+    Ok(())
+}
+
+fn parse(input: &str) -> Result<ast::program::Program, String> {
+    match parser::ProgramParser::new().parse(lexer::Lexer::new(input.char_indices())) {
+        Ok(s) => Ok(s),
+        Err(e) => Err(format!("{:?}", e)),
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,17 @@
-use std::fs;
-
-use ast::Generate;
-
 #[macro_use]
 extern crate lalrpop_util;
+
+mod cli;
 
 pub mod ast;
 pub mod lexer;
 
 lalrpop_mod!(pub parser);
 
-struct Args {
-    filename: Option<String>,
-}
-
-fn parse(input: &str) -> Result<ast::program::Program, String> {
-    match parser::ProgramParser::new().parse(lexer::Lexer::new(input.char_indices())) {
-        Ok(s) => Ok(s),
-        Err(e) => Err(format!("{:?}", e)),
-    }
-}
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut args = pico_args::Arguments::from_env();
+    let args = cli::get_arguments()?;
 
-    let args = Args {
-        filename: args.opt_value_from_str("--filename")?,
-    };
-
-    let filename = match args.filename {
-        Some(f) => f,
-        None => panic!("Please enter a filename using the --filename argument."),
-    };
-
-    let program_code: String = fs::read_to_string(&filename).expect("Failed to read the file.");
-
-    let ast = parse(&program_code).expect("Failed to parse the given program");
-    dbg!(&ast);
-
-    println!("Program: {}", ast.generate());
-
-    Ok(())
+    cli::process_args(args)
 }
 
 #[cfg(test)]
