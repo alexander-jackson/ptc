d00f3a12319171dae5f5ebf36763189f4ca46e7d
Alexander Jackson <alexanderjackson@protonmail.com>
2019-12-26 21:58:16 +0100

    Move each test onto a single line

--- src/tests/parser_tests.rs
+++ src/tests/parser_tests.rs
@@ -25,49 +25,16 @@ parser_success! {
     parse_integers: "40\n",
     parse_expressions: "var = 1 + 1 - 1 * 1 / 1\n",
     parse_bracketed_expressions: "var = (1 - 1) * 1\n",
-    parse_comparison_operators: r#"
-var = 0 < 1
-var = 0 > 1
-var = 0 <= 1
-var = 0 >= 1
-var = 0 == 1
-var = 0 != 1
-"#,
-    parse_augmented_assignments: r#"
-var += 1
-var -= 1
-var *= 1
-var /= 1
-"#,
+    parse_comparison_operators: "var = 0 < 1\nvar = 0 > 1\nvar = 0 <= 1\nvar = 0 >= 1\nvar = 0 == 1\nvar = 0 != 1\n",
+    parse_augmented_assignments: "var += 1\nvar -= 1\nvar *= 1\nvar /= 1\n",
     parse_identifier_expression: "var = var_one + var_two\n",
     parse_function_call: "var = add(1, 2)\n",
     parse_logical_expressions: "a and b\na or b\nnot a\n",
     parse_pass_statement: "pass\n",
-    parse_if_statement: r#"
-if 1:
-    pass
-"#,
-    parse_if_else_statement: r#"
-if 1:
-    pass
-else:
-    pass
-"#,
-    parse_while_statement: r#"
-while 1:
-    pass
-"#,
-    parse_nested_compound_statements: r#"
-while expression:
-    if other_expression:
-        pass
-"#,
-    parse_function_declaration: r#"
-def useless(x, y):
-    pass
-"#,
-    parse_return_statement: r#"
-def add(x, y):
-    return x + y
-"#,
+    parse_if_statement: "if 1:\n    pass\n",
+    parse_if_else_statement: "if 1:\n    pass\nelse:\n    pass\n",
+    parse_while_statement: "while 1:\n    pass\n",
+    parse_nested_compound_statements: "while expression:\n    if other_expression:\n        pass\n",
+    parse_function_declaration: "def useless(x, y):\n    pass\n",
+    parse_return_statement: "def add(x, y):\n    return x + y\n",
 }
