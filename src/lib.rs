651ca9e1de2376dcac9c4157d8aed8dfe6e2ffc7
Alexander Jackson <alexanderjackson@protonmail.com>
2020-01-10 10:33:47 +0000

    Remove error_handling and move into lib.rs
    Remove the error_handling module and move the code into lib.rs anyway as it was
    overly simple.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@
 extern crate lalrpop_util;
 
 mod cli;
-mod error_handling;
 
 pub mod ast;
 pub mod lexer;
@@ -11,5 +10,9 @@ lalrpop_mod!(pub parser);
 
 pub fn lib_main() {
     let result = cli::get_arguments().and_then(cli::process_args);
-    error_handling::handle_errors(result);
+
+    match result {
+        Ok(_) => (),
+        Err(e) => eprintln!("Error occurred: {}", e),
+    };
 }
