9bd92d2c2ea7b78e079a8850c29e582775e2da88
Alexander Jackson <alexanderjackson@protonmail.com>
2020-01-31 10:02:10 +0000

    Remove variable_types from Context
    Remove the variable_types field from Context and move the functionality into
    symbol_table instead, storing a HashMap<String, VariableType> instead.

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -51,46 +51,42 @@ pub trait Type {
 
 #[derive(Debug)]
 pub struct Context {
-    symbol_table: Vec<HashSet<String>>,
-    variable_types: Vec<HashMap<String, VariableType>>,
+    symbol_table: Vec<HashMap<String, Option<VariableType>>>,
 }
 
 impl Context {
     pub fn new() -> Context {
         Context {
-            symbol_table: vec![HashSet::new()],
-            variable_types: vec![HashMap::new()],
+            symbol_table: vec![HashMap::new()],
         }
     }
 
     pub fn push_scope(&mut self) {
-        self.symbol_table.push(HashSet::new());
-        self.variable_types.push(HashMap::new());
+        self.symbol_table.push(HashMap::new());
     }
 
     pub fn pop_scope(&mut self) {
         self.symbol_table.pop();
-        self.variable_types.pop();
     }
 
     pub fn contains(&self, variable: &str) -> bool {
-        self.symbol_table.iter().any(|x| x.contains(variable))
+        self.symbol_table.iter().any(|x| x.contains_key(variable))
     }
 
     pub fn insert(&mut self, variable: &str) {
         let index: usize = self.symbol_table.len() - 1;
-        self.symbol_table[index].insert(variable.to_string());
+        self.symbol_table[index].insert(variable.to_string(), None);
     }
 
     pub fn insert_inferred_type(&mut self, variable: &str, inferred: VariableType) {
-        let index: usize = self.variable_types.len() - 1;
-        self.variable_types[index].insert(variable.to_string(), inferred);
+        let index: usize = self.symbol_table.len() - 1;
+        self.symbol_table[index].insert(variable.to_string(), Some(inferred));
     }
 
     pub fn get_type(&self, variable: &str) -> Option<VariableType> {
-        for m in self.variable_types.iter().rev() {
+        for m in self.symbol_table.iter().rev() {
             if let Some(v) = m.get(variable) {
-                return Some(*v);
+                return *v;
             }
         }
 
