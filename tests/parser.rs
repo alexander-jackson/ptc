0daae1cae609a787533bcac3e350415c6f3fd055
Alexander Jackson <alexanderjackson@protonmail.com>
2020-02-10 10:13:33 +0000

    Add 2 tests and cleanup others

--- tests/parser.rs
+++ tests/parser.rs
@@ -36,13 +36,14 @@ macro_rules! parser_failure {
 parser_success! {
     parse_underscores_in_identifiers: "longer_name\n",
     parse_integers: "40\n",
+    parse_floats: "40.0\n",
     parse_expressions: "var = 1 + 1 - 1 * 1 / 1 % 1\n",
     parse_unary_expressions: "+var\n-var\n",
     parse_bracketed_expressions: "var = (1 - 1) * 1\n",
-    parse_comparison_operators: "var = 0 < 1\nvar = 0 > 1\nvar = 0 <= 1\nvar = 0 >= 1\nvar = 0 == 1\nvar = 0 != 1\n",
+    parse_comparison_operators: "0 < 1\n0 > 1\n0 <= 1\n0 >= 1\n0 == 1\n0 != 1\n",
     parse_augmented_assignments: "var += 1\nvar -= 1\nvar *= 1\nvar /= 1\nvar %= 1\n",
-    parse_identifier_expression: "var = var_one + var_two\n",
-    parse_function_call: "var = add(1, 2)\n",
+    parse_identifier_expression: "var_one + var_two\n",
+    parse_function_call: "add(1, 2)\n",
     parse_logical_expressions: "a and b\na or b\nnot a\n",
     parse_pass_statement: "pass\n",
     parse_if_statement: "if 1:\n    pass\n",
@@ -59,4 +60,5 @@ parser_failure! {
     fail_compound_without_indentation: "if 1:\npass\n",
     fail_function_call_with_trailing_comma: "add(1, 2,)\n",
     fail_function_definition_with_trailing_comma: "def add(x, y,):\n    pass\n",
+    fail_mixed_indentation: "if 1:\n\tpass\nelse:\n    pass\n",
 }
