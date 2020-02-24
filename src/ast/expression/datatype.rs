8386d58d69417cb14b6d338d66e0dd450363b8b5
Alexander Jackson <alexanderjackson@protonmail.com>
2020-02-24 18:57:34 +0000

    Add type inference for UnaryOperations

--- src/ast/expression/datatype.rs
+++ src/ast/expression/datatype.rs
@@ -1,9 +1,17 @@
 use ast::Expression;
-use ast::{Context, DataType, Generate, VariableType};
+use ast::{Context, DataType, Generate, Operator, VariableType};
 
 impl DataType for Expression {
     fn get_type(&self, context: &mut Context) -> VariableType {
         match self {
+            Expression::UnaryOperation { op, expr } => {
+                match op {
+                    Operator::Plus => expr.get_type(context),
+                    Operator::Minus => expr.get_type(context),
+                    Operator::LogicalNot => VariableType::Integer,
+                    _ => VariableType::Unknown,
+                }
+            }
             Expression::Literal { value } => value.get_type(context),
             Expression::Identifier { name } => name.get_type(context),
             Expression::FunctionCall { name, .. } => {
